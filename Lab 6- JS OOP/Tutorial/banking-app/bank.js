85f516612b9bcaa08e730b9cccd72cd7d4084368
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-02-24 14:50:33 +0300

    updated

---
+++ Lab 6- JS OOP/Tutorial/banking-app/bank.js
@@ -0,0 +1,16 @@
+const accounts = [
+    { accountNo: 123, balance: 500, type: 'Savings' },
+    { accountNo: 234, balance: 4000, type: 'Current' },
+    { accountNo: 345, balance: 35000, type: 'Savings' },
+]
+
+// function deposit(accountNo, amount)
+// function withdraw(accountNo, amount)
+// function addAccount(accountNo, balance, type)
+// function getAccount(accountNo)
+// function avgBalance()
+// function sumBalance()
+// function distributeBenefit(benefitPercentage)
+// function deductFee(monthlyFee)
+// function toJson()
+// function fromJson(json)
