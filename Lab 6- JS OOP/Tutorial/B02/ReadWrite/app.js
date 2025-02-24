da5080f71bb72fbf19cde2766c4955ed75ce84d6
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-02-24 17:53:40 +0300

    update

--- Lab 6- JS OOP/Tutorial/B02/ReadWrite/app.js
+++ Lab 6- JS OOP/Tutorial/B02/ReadWrite/app.js
@@ -8,3 +8,20 @@ const name = input("What is your name? ");
 console.log(`Hello Mr./Ms., ${name}!`);
 
 // SyntaxError: Cannot use import statement outside a module
+
+const students = [
+    { name: 'Ahmed', gender: 'Male', grade: 85, age: 23 },
+    { name: 'Sara', gender: 'Female', grade: 77, age: 31 },
+    { name: 'Hassen', gender: 'Male', grade: 92, age: 29 },
+    { name: 'Zahra', gender: 'Female', grade: 95, age: 20 },
+    { name: 'Abdulahi', gender: 'Male', grade: 54, age: 22 }
+]
+
+const theOlderstStudentName = students.reduce((prev, current) => {
+    return (prev.age > current.age) ? prev : current
+}).name;
+
+const avg = students.reduce((sum, current) => sum + current.age, 0) / students.length;
+
+
+console.log(`The oldest student is ${theOlderstStudentName}`);
---
+++ Lab 6- JS OOP/Tutorial/B02/banking-app-module/bank.js
@@ -0,0 +1,7 @@
+const accounts = [
+    { accountNo: 123, balance: 1000, type: 'Savings', name: 'John' },
+    { accountNo: 124, balance: 2000, type: 'Current', name: 'Jane' },
+    { accountNo: 125, balance: 3000, type: 'Savings', name: 'Doe' },
+    { accountNo: 126, balance: 4000, type: 'Current', name: 'Smith' },
+    { accountNo: 127, balance: 5000, type: 'Savings', name: 'Alex' },
+]
