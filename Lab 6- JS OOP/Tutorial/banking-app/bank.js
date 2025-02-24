977f672d7fc1a8768052bc903695bc7ee83bb9eb
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-02-24 15:00:00 +0300

    updated

--- Lab 6- JS OOP/Tutorial/banking-app/bank.js
+++ Lab 6- JS OOP/Tutorial/banking-app/bank.js
@@ -4,13 +4,46 @@ const accounts = [
     { accountNo: 345, balance: 35000, type: 'Savings' },
 ]
 
-// function deposit(accountNo, amount)
-// function withdraw(accountNo, amount)
-// function addAccount(accountNo, balance, type)
-// function getAccount(accountNo)
-// function avgBalance()
+function deposit(accountNo, amount) {
+    const account = accounts.find(acc => acc.accountNo === accountNo);
+    account.balance += amount;
+}
+
+function withdraw(accountNo, amount) {
+    const account = accounts.find(acc => acc.accountNo === accountNo);
+
+    if (account.balance >= amount)
+        account.balance -= amount;
+    else
+        console.log('Insufficient balance');
+
+}
+
+function addAccount(accountNo, balance, type) {
+    accounts.push({ accountNo, balance, type });
+}
+function getAccount(accountNo) {
+    return accounts.find(acc => acc.accountNo === accountNo);
+}
 // function sumBalance()
+// function avgBalance()
+
 // function distributeBenefit(benefitPercentage)
 // function deductFee(monthlyFee)
 // function toJson()
 // function fromJson(json)
+
+
+/*
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((acc, num) => acc + num, 0);
+
+const objects = [
+    { name: 'John', age: 20 },
+    { name: 'Jane', age: 30 },
+    { name: 'Jack', age: 40 },
+]
+
+const sum = numbers.reduce((acc, obj) => acc + obj.age, 0);
+*/
