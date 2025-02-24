f214f4892ba2155667ceaa51b1efcddbbf9782ee
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-02-24 14:16:15 +0300

    lab 6

---
+++ Lab 6- JS OOP/UnitConverter/unit-converter.spec.js
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+// we need to write the describe method and add its it
+
+describe("Unit Converter", () => {
+    it("should convert km to miles", () => {
+        expect(1).to.equal(1);
+    });
+});
