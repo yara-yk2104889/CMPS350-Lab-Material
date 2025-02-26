d086fae4104cecf42afc2f8547795e8dff29a5a5
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-02-26 10:01:39 +0300

    banking app

---
+++ Lab 6- JS OOP/Tutorial/B52/banking-app/bank.js
@@ -0,0 +1,6 @@
+const accounts = [
+    { accountNo: 111, balance: 1000, type: 'Savings', name: 'Ali' },
+    { accountNo: 121, balance: 2000, type: 'Current', name: 'Omar' },
+    { accountNo: 131, balance: 3000, type: 'Savings', name: 'Mohamed' },
+    { accountNo: 141, balance: 4000, type: 'Current', name: 'Yusuf' },
+]
