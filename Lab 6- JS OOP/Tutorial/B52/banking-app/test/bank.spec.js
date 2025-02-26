0ce72ccd774257f9b9c449c6c803388acc2406df
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-02-26 11:03:32 +0300

    updated

---
+++ Lab 6- JS OOP/Tutorial/B52/banking-app/test/bank.spec.js
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { addAccount, getAccounts, getAccount } from "../bank.js";
+import { it } from "mocha";
+
+
+// used for grouping tests together
+describe("The bank", () => {
+    it("adding account to increase the size of the array by 1", () => {
+        const initialSize = getAccounts().length;
+        addAccount({ accountNo: 555, balance: 12000, type: 'Savings', name: 'Layan' })
+        const finalSize = getAccounts().length;
+        expect(finalSize).to.equal(initialSize + 1);
+    });
+
+    it("Withdraw 1000 from account 111", () => {
+        const account = getAccount(111);
+        
+        const initialBalance = account.balance;
+        withdraw(111, 1000);
+        const finalBalance = account.balance;
+        expect(finalBalance).to.equal(initialBalance - 1000);
+    });
+
+});
