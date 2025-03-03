4acb8e9069275855b1b0b1519490ca31e28fe35a
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 15:27:05 +0300

    updated code

--- Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
@@ -15,14 +15,52 @@ countryDD.addEventListener("change", handleCountryChange);
 // write the function to handle the region change event
 
 async function handleRegionChange() {
-
     const url = `${REGION_BASE_URL}${regionDD.value}`;
     const response = await fetch(url);
     const data = await response.json();
-    const countryNames = data.map(country => country.name.common);
-    const countryOptions = countryNames
-        .map(name => `<option value="${name}">${name}</option>`);
+    const countryOptions = data
+        .map(country => `<option value="${country.name.common}">${country.name.official}</option>`);
+
     countryDD.innerHTML = countryOptions.join(' ');
 }
 
 
+async function handleCountryChange() {
+    const url = `${COUNTRY_BASE_URL}${countryDD.value}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const country = data[0];
+    factsArea.innerHTML = convertCountryToHtml(country);
+}
+
+function convertCountryToHtml(country) {
+    const currencies = Object.values(country.currencies)
+        .map(currency => currency.name).join(', ');
+    return `
+       <h1>${country.name.official}</h1>
+       <img src="${country.flags.png}" alt="${country.flags.alt}" /> 
+
+       <table>
+              <tr>
+                <th>Capital</td>
+                <td>${country.capital}</td>
+              </tr>
+              <tr>
+                <th>Area</td>
+                <td>${country.area} km<sup>2</sup></td>
+              </tr>
+              <tr>
+                <th>Population</td>
+                <td>${country.population}</td>
+              </tr>
+              <tr>
+                <th>Languages</td>
+                <td>${Object.values(country.languages).join(', ')}</td>
+              </tr>
+              <tr>
+                <th>Currencies</td>
+                <td>${currencies}</td>
+              </tr>
+       </table>
+    `
+}
