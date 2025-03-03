e655e45e4d5889c8279aaf305f12aa6ea37188c5
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 14:51:33 +0300

    update

---
+++ Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
@@ -0,0 +1,23 @@
+// Base URLS
+// check if the dom is loaded
+const REGION_BASE_URL = "https://restcountries.com/v3.1/region/";
+const COUNTRY_BASE_URL = "https://restcountries.com/v3.1/name/";
+
+const regionDD = document.querySelector("#region");
+const countryDD = document.querySelector("#country");
+const factsArea = document.querySelector("#facts-area");
+const mainContent = document.querySelector("#main-content");
+
+// Listen to events on the region dropdown
+regionDD.addEventListener("change", handleRegionChange);
+countryDD.addEventListener("change", handleCountryChange);
+
+// write the function to handle the region change event
+
+async function handleRegionChange() {
+    alert("Region Changed");
+    const url = `${REGION_BASE_URL}${regionDD.value}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
