eb0385560d8b8bd52e4235ae9d550fd9e37823b4
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 22:46:21 +0300

    updated

--- Lab 7 - Client JS/Tutorial/B02/TodoListApp/js/index.js
+++ Lab 7 - Client JS/Tutorial/B02/TodoListApp/js/index.js
@@ -11,7 +11,9 @@ form.addEventListener('submit', handleSubmit);
 clearTodos.addEventListener('click', handleClearTodos);
 
 // create a function to handle the submit event
-let todos = [];
+let todos = localStorage.todos ? JSON.parse(localStorage.todos) : [];
+
+renderTodos();
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -20,7 +22,11 @@ function handleSubmit(e) {
     console.log(data);
 
     const todo = Object.fromEntries(data);
+    todo.id = `id-${Date.now()}`;
     todos.push(todo);
+
+    // step 1: save the todos to the local storage
+    localStorage.todos = JSON.stringify(todos);
 
     renderTodos();
     form.reset();
@@ -31,16 +37,30 @@ function renderTodos() {
 }
 
 function covertToCard(todo) {
-    return ` <div class="form-group todo" id="">
-                    <p class="todo-title" id="">${todo.title}</p>
-                    <input class="completed icon" type="checkbox"
-                    onclick="" ${todo.completed ? 'checked' : ''}>
-                    <i class="fa fa-trash icon" onclick=""></i>
-                    </div>
+    return ` 
+        <div class="form-group todo" id="${todo.id}">
+            <p class="todo-title ${todo.completed ? 'strike' : ''}" id="">${todo.title}</p>
+            <input class="completed icon" type="checkbox" onclick="" ${todo.completed ? 'checked' : ''}
+                onchange="toggleCompleted('${todo.id}')">
+            <i class="fa fa-trash icon" onclick="deleteTodo('${todo.id}')"></i>
+        </div>
             `;
 }
 
 function handleClearTodos() {
     todos = [];
+    delete localStorage.todos;
+    renderTodos();
+}
+
+function deleteTodo(id) {
+    const index = todos.findIndex(todo => todo.id === id);
+    todos.splice(index, 1);
+    renderTodos();
+}
+
+function toggleCompleted(id) {
+    const index = todos.findIndex(todo => todo.id === id);
+    todos[index].completed = !todos[index].completed;
     renderTodos();
 }
