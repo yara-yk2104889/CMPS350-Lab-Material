65d34b84d8a799e2f6d4c7728e8a2f03e3d3eab4
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 20:41:49 +0300

    updated

--- Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
@@ -1,3 +1,15 @@
-// access an element in my html that is not loaded yet //error
+// Get a reference to the elements you want to interact with
 
-const regionsDD = document.querySelector('#regions');
+const regionsDD = document.querySelector('#region');
+const countryDD = document.querySelector('#country');
+const factsArea = document.querySelector('#facts-area');
+
+// Add events listeners to the different elements
+
+regionsDD.addEventListener('change', handleRegionsChange);
+
+
+function handleRegionsChange() {
+    alert('Regions dropdown changed');
+
+}
