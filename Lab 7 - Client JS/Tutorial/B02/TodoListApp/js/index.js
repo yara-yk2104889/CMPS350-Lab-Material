89b1fc2f5c3595486fc2967f5cf879dae6734689
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 22:29:26 +0300

    up

--- Lab 7 - Client JS/Tutorial/B02/TodoListApp/js/index.js
+++ Lab 7 - Client JS/Tutorial/B02/TodoListApp/js/index.js
@@ -4,13 +4,15 @@
 
 const form = document.querySelector('#form');
 const todoListContainer = document.querySelector('#todolist-container');
-
+const clearTodos = document.querySelector('#clear-todos');
 
 // add event listener to the form
 form.addEventListener('submit', handleSubmit);
+clearTodos.addEventListener('click', handleClearTodos);
 
 // create a function to handle the submit event
 let todos = [];
+
 function handleSubmit(e) {
     e.preventDefault();
     // const data = new FormData(form);
@@ -30,10 +32,15 @@ function renderTodos() {
 
 function covertToCard(todo) {
     return ` <div class="form-group todo" id="">
-                    <p class="todo-title" id=""></p>
+                    <p class="todo-title" id="">${todo.title}</p>
                     <input class="completed icon" type="checkbox"
-                    onclick="">
+                    onclick="" ${todo.completed ? 'checked' : ''}>
                     <i class="fa fa-trash icon" onclick=""></i>
                     </div>
             `;
+}
+
+function handleClearTodos() {
+    todos = [];
+    renderTodos();
 }
