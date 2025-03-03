ca1c4af572669c574afb6e35cc687d2ea4fc2e52
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 20:42:18 +0300

    up

--- Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
@@ -1,12 +1,12 @@
 // Get a reference to the elements you want to interact with
 
-const regionsDD = document.querySelector('#region');
+const regionDD = document.querySelector('#region');
 const countryDD = document.querySelector('#country');
 const factsArea = document.querySelector('#facts-area');
 
 // Add events listeners to the different elements
 
-regionsDD.addEventListener('change', handleRegionsChange);
+regionDD.addEventListener('change', handleRegionsChange);
 
 
 function handleRegionsChange() {
