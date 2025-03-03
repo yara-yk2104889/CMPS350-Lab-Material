a45fc223d10118dd4042c1d4794bca8b03c15923
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 21:24:29 +0300

    upt o here

--- Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
@@ -11,6 +11,7 @@ const factsArea = document.querySelector('#facts-area');
 // Add events listeners to the different elements
 
 regionDD.addEventListener('change', handleRegionsChange);
+countryDD.addEventListener('change', handleCountryChange);
 
 
 async function handleRegionsChange(e) {
@@ -20,9 +21,22 @@ async function handleRegionsChange(e) {
     const countries = await response.json();
     const countryOptions = countries.map(country => `<option value="${country.name.common}">${country.name.official}</option>`);
     countryDD.innerHTML = countryOptions.join(' ');
-
-
-
 }
 
 
+async function handleCountryChange(e) {
+    const url = `${countryBaseUrl}${countryDD.value}`;
+    console.log(url);
+    const response = await fetch(url);
+    const data = await response.json();
+    const country = data[0];
+    console.log(country);
+    factsArea.innerHTML = convertCountryToHtml(country);
+}
+
+function convertCountryToHtml(country) {
+    return `
+        <h1>Facts about ${country.name.common}</h1>
+        <img src="${country.flags.png}" alt="${country.flags.alt}" />
+    `
+}
