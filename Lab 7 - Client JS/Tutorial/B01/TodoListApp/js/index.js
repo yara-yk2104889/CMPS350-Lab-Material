c590232742e9f401e5dfcbbea6cb6f042c8c5320
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 16:52:47 +0300

    up

--- Lab 7 - Client JS/Tutorial/B01/TodoListApp/js/index.js
+++ Lab 7 - Client JS/Tutorial/B01/TodoListApp/js/index.js
@@ -3,26 +3,48 @@ const todolistContainer
     = document.querySelector('#todolist-container');
 const clearTodos = document.querySelector('#clear-todos');
 
-const todos = [];
+let todos = (localStorage.todos ? JSON.parse(localStorage.todos) : []);
+renderTodos();
 // Add event listener to form
 form.addEventListener('submit', handleSubmit)
+
+clearTodos.addEventListener('click', handleClearTodos);
+
+function handleClearTodos() {
+    todos = [];
+    delete localStorage.todos
+    renderTodos();
+}
 
 function handleSubmit(e) {
     e.preventDefault();
     const data = new FormData(e.target);
     const todo = Object.fromEntries(data);
-    todos.push(todo);
-    todolistContainer.innerHTML = todos.map(todo => renderTodo(todo)).join('');
+    todo.id = `id-${Date.now()}`;
+    console.log(todo);
 
+    todos.push(todo);
+    renderTodos();
+    form.reset();
+
+    // save the todos into local storage
+    localStorage.todos = JSON.stringify(todos);
+}
+function renderTodos() {
+    todolistContainer.innerHTML = todos.map(todo => renderTodo(todo)).join('');
 }
 
 function renderTodo(todo) {
     console.log(todo);
 
-    return ` <div class="form-group todo" id="">
-                <p class="todo-title" id="">${todo.title}</p>
+    return ` <div class="form-group todo" id="${todo.id}">
+                <p class="todo-title ${todo.completed ? 'strike' : ''}" id="">${todo.title}</p>
                 <input class="completed icon" type="checkbox"
                 onclick="" ${todo.completed ? 'checked' : ''}>
-                <i class="fa fa-trash icon" onclick=""></i>
-            </div>`;
+                <i class="fa fa-trash icon" onclick="deleteTodo('${todo.id}')"></i>
+            </div> `;
+}
+
+function deleteTodo(id) {
+    document.querySelector(`#${id}`).remove();
 }
