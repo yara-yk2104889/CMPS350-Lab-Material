293d492cb46134d1bf916e0e0b5eeee842bc18d3
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 21:43:13 +0300

    updated

--- Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/B02/Country Explorer/js/index.js
@@ -26,17 +26,39 @@ async function handleRegionsChange(e) {
 
 async function handleCountryChange(e) {
     const url = `${countryBaseUrl}${countryDD.value}`;
-    console.log(url);
     const response = await fetch(url);
     const data = await response.json();
     const country = data[0];
-    console.log(country);
     factsArea.innerHTML = convertCountryToHtml(country);
 }
 
 function convertCountryToHtml(country) {
+
+    const currencies = Object.values(country.currencies)
+        .map(currency => currency.name)
+        .join(' - ');
+    const languages = Object.values(country.languages).join(' - ');
     return `
         <h1>Facts about ${country.name.common}</h1>
         <img src="${country.flags.png}" alt="${country.flags.alt}" />
+        <table>
+            <tr>
+                <th>Official Country Name</th>
+                <td>${country.name.official}</td>
+            </tr>
+            <tr>
+                <th>Capital</th>
+                <td>${country.capital}</td>
+            </tr>
+            <tr>
+                <th>Currencies</th>
+                <td>${currencies}</td>
+            </tr>
+            <tr>
+                <th>Languages</th>
+                <td>${languages}</td>
+            </tr>
+          
+        </table>
     `
 }
