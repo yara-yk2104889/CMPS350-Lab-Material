e9546cadce536966cd9887d4fa5636fc4051d3c0
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-03 22:01:45 +0300

    update

--- Lab 7 - Client JS/Tutorial/B01/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/B01/Country Explorer/js/index.js
@@ -3,6 +3,7 @@
 const REGION_BASE_URL = "https://restcountries.com/v3.1/region/";
 const COUNTRY_BASE_URL = "https://restcountries.com/v3.1/name/";
 
+// get the references that you want to work with
 const regionDD = document.querySelector("#region");
 const countryDD = document.querySelector("#country");
 const factsArea = document.querySelector("#facts-area");
@@ -14,29 +15,29 @@ countryDD.addEventListener("change", handleCountryChange);
 
 // write the function to handle the region change event
 
-async function handleRegionChange() {
-    const url = `${REGION_BASE_URL}${regionDD.value}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const countryOptions = data
-        .map(country => `<option value="${country.name.common}">${country.name.official}</option>`);
+async function handleRegionChange(e) {
+  const url = `${REGION_BASE_URL}${regionDD.value}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const countryOptions = data
+    .map(country => `<option value="${country.name.common}">${country.name.official}</option>`);
 
-    countryDD.innerHTML = countryOptions.join(' ');
+  countryDD.innerHTML = countryOptions.join(' ');
 }
 
 
 async function handleCountryChange() {
-    const url = `${COUNTRY_BASE_URL}${countryDD.value}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const country = data[0];
-    factsArea.innerHTML = convertCountryToHtml(country);
+  const url = `${COUNTRY_BASE_URL}${countryDD.value}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const country = data[0];
+  factsArea.innerHTML = convertCountryToHtml(country);
 }
 
 function convertCountryToHtml(country) {
-    const currencies = Object.values(country.currencies)
-        .map(currency => currency.name).join(', ');
-    return `
+  const currencies = Object.values(country.currencies)
+    .map(currency => currency.name).join(', ');
+  return `
        <h1>${country.name.official}</h1>
        <img src="${country.flags.png}" alt="${country.flags.alt}" /> 
 
