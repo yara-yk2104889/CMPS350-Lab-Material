b647eb30e27b3a1bce5036a97c278214ab300ed7
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-05 11:03:08 +0300

    updated

--- Lab 7 - Client JS/Tutorial/B52/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/B52/Country Explorer/js/index.js
@@ -5,17 +5,18 @@
 const regionsEndPoint = "https://restcountries.com/v3.1/region/";
 const countryEndPoint = "https://restcountries.com/v3.1/name/";
 
-// Get the references to the DOM elements
+// 1. Get the references to the DOM elements
 
 const regionDD = document.querySelector("#region");
 const countryDD = document.querySelector("#country");
 const factsArea = document.querySelector("#facts-area");
 
 
-// add event listener 
+// 2. add event listener 
 regionDD.addEventListener('change', handleRegionChange);
+countryDD.addEventListener('change', handleCountryChange);
 
-// write the function to handle the change event
+// 3. write the function to handle the change event
 
 async function handleRegionChange(e) {
     // get the countries of the selected region from an API end point
@@ -29,5 +30,37 @@ async function handleRegionChange(e) {
 
     countryDD.innerHTML = countryOptions;
 
+}
 
+
+async function handleCountryChange(e) {
+    const url = `${countryEndPoint}${countryDD.value}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    factsArea.innerHTML = convertCountryToHTML(data[0]);
+}
+
+function convertCountryToHTML(country) {
+    const currencies = Object.values(country.currencies).map(currency => currency.name).join(", ");
+    console.log(currencies);
+
+    return `
+        <h1> ${country.name.common}</h1> <br>
+        <img src="${country.flags.png}" alt="${country.flags.alt}" width="100" height="100">
+
+        <table>
+            <tr>
+                <th>Official Name</th>
+                <td> ${country.name.official}</td>
+            </tr>
+            <tr>
+                <th>Population</th>
+                <td> ${country.population}</td>
+            </tr>
+            <tr>
+                <th>Currencies</th>
+                <td> ${currencies}</td>
+            </tr>
+        </table>
+        `
 }
