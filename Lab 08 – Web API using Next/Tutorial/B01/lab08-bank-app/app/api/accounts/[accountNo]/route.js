fcce5bb553249507d0af677d84788caacb354e90
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-10 15:27:51 +0300

    upto the APIs

--- Lab 08 – Web API using Next/Tutorial/B01/lab08-bank-app/app/api/accounts/[accountNo]/route.js
+++ Lab 08 – Web API using Next/Tutorial/B01/lab08-bank-app/app/api/accounts/[accountNo]/route.js
@@ -2,14 +2,14 @@
 export async function GET(req, { params }) {
 
     const accountNo = params.accountNo;
-    return Response.json({ message: 'GET /api/accounts/{accountNo}', accountNo });
+    return Response.json({ message: `GET /api/accounts/${accountNo}` });
 }
 // UPDATE EXISTING RECORD
 export async function PUT(req, { params }) {
     const { accountNo } = params;
-    return Response.json({ message: 'PUT /api/accounts/{accountNo}' });
+    return Response.json({ message: `PUT /api/accounts/${accountNo}` });
 }
 // DELETE RECORD
-export async function DELETE(req) {
-    return Response.json({ message: 'DELETE /api/accounts/{accountNo}' });
+export async function DELETE(req, { params }) {
+    return Response.json({ message: `DELETE /api/accounts/${accountNo}` });
 }
--- Lab 08 – Web API using Next/Tutorial/B01/lab08-bank-app/app/api/accounts/route.js
+++ Lab 08 – Web API using Next/Tutorial/B01/lab08-bank-app/app/api/accounts/route.js
@@ -1,7 +1,14 @@
+const accounts = [
+    { id: 1, name: 'Account 1' },
+    { id: 2, name: 'Account 2' },
+    { id: 3, name: 'Account 3' },
+    { id: 4, name: 'Account 4' },
+    { id: 5, name: 'Account 5' },
+]
 export async function GET(req) {
     const { searchParams } = new URL(req.url);
     const { type } = Object.fromEntries(searchParams.entries());
-    return Response.json({ message: 'GET /api/accounts?type=', type });
+    return Response.json(accounts);
 }
 export async function POST(req) {
     return Response.json({ message: 'POST /api/accounts' });
