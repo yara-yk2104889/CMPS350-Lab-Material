b96cdc68fdef2855d38222328200030758d64941
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-10 15:28:55 +0300

    updated

--- Lab 08 – Web API using Next/Tutorial/B01/lab08-bank-app/app/api/accounts/route.js
+++ Lab 08 – Web API using Next/Tutorial/B01/lab08-bank-app/app/api/accounts/route.js
@@ -1,10 +1,3 @@
-const accounts = [
-    { id: 1, name: 'Account 1' },
-    { id: 2, name: 'Account 2' },
-    { id: 3, name: 'Account 3' },
-    { id: 4, name: 'Account 4' },
-    { id: 5, name: 'Account 5' },
-]
 export async function GET(req) {
     const { searchParams } = new URL(req.url);
     const { type } = Object.fromEntries(searchParams.entries());
