0546bb70627557be3f477505dcb96fab0ca596d9
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-10 21:42:32 +0300

    updated

--- Lab 08 – Web API using Next/Tutorial/B02/lab08-banking-app/app/api/accounts/route.js
+++ Lab 08 – Web API using Next/Tutorial/B02/lab08-banking-app/app/api/accounts/route.js
@@ -1,28 +1,15 @@
-// under each route you can have the following methods: GET, POST, PUT, DELETE , PATCH etc
-import { nanoid } from 'nanoid';
-const accounts = [
-    { accountNo: 1, name: 'John Doe', balance: 1000, type: 'savings' },
-    { accountNo: 2, name: 'Jane Doe', balance: 2000, type: 'current' },
-    { accountNo: 3, name: 'John Smith', balance: 3000, type: 'savings' },
-    { accountNo: 4, name: 'Jane Smith', balance: 4000, type: 'current' },
-]
+import accountRepo from "@/app/repo/account-repo";
 
 export async function GET(req) {
     const { searchParams } = new URL(req.url);
     const { type } = Object.fromEntries(searchParams.entries())
-    if (type) {
-        const filteredAccounts = accounts.filter(account => account.type == type);
-        return Response.json(filteredAccounts, { status: 200 });
-    }
-
+    const accounts = await accountRepo.getAccounts(type);
     return Response.json(accounts, { status: 200 });
 }
 
 export async function POST(req) {
     //capture the request body
     const account = await req.json();
-    //generate a unique id
-    account.accountNo = nanoid();
-    accounts.push(account);
-    return Response.json(account, { status: 201 });
+    const newAccount = await accountRepo.createAccount(account);
+    return Response.json(newAccount, { status: 201 });
 }
