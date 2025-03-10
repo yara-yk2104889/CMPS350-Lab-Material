bcacb6c87c4f1fd3108edec5c71b4abbf11d12a1
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-10 20:42:43 +0300

    updated

--- Lab 08 – Web API using Next/Tutorial/B02/lab08-banking-app/app/api/accounts/route.js
+++ Lab 08 – Web API using Next/Tutorial/B02/lab08-banking-app/app/api/accounts/route.js
@@ -1,4 +1,5 @@
 // under each route you can have the following methods: GET, POST, PUT, DELETE , PATCH etc
+import { nanoid } from 'nanoid';
 const accounts = [
     { id: 1, name: 'John Doe', balance: 1000 },
     { id: 2, name: 'Jane Doe', balance: 2000 },
@@ -6,4 +7,13 @@ const accounts = [
 ]
 export async function GET(req) {
     return Response.json(accounts, { status: 200 });
+}
+
+export async function POST(req) {
+    //capture the request body
+    const account = await req.json();
+    //generate a unique id
+    account.id = nanoid();
+    accounts.push(account);
+    return Response.json(account, { status: 201 });
 }
