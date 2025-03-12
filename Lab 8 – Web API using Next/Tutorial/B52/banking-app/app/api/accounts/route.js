39acdbb78b85dfb728722bf37639efdd8396660e
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-12 11:38:51 +0300

    upto get

--- Lab 8 – Web API using Next/Tutorial/B52/banking-app/app/api/accounts/route.js
+++ Lab 8 – Web API using Next/Tutorial/B52/banking-app/app/api/accounts/route.js
@@ -1,23 +1,14 @@
-import account
+import accountRepo from '@/app/repo/account-repo'
 export async function GET(req) {
     const { searchParams } = new URL(req.url)
+    const type = searchParams.get('type')
 
+    const accounts = await accountRepo.getAccounts(type)
+    return Response.json(accounts, { status: 200 })
 }
 
-// ADD NEW ACCOUNT [CREATE A NEW ACCOUNT]
 export async function POST(req) {
-    const data = await req.json()
-
-    return Response.json({
-        message: 'You are in the accounts POST', data
-    }, { status: 200 })
-}
-
-// UPDATE ACCOUNT [UPDATE AN ACCOUNT]
-export async function PUT(req) {
-    return Response.json({ message: 'You are in the accounts PUT' }, { status: 200 })
-}
-
-export async function DELETE(req) {
-    return Response.json({ message: 'You are in the accounts DELETE' }, { status: 200 })
+    const account = await req.json()
+    const newAccount = await accountRepo.createAccount(account)
+    return Response.json(newAccount, { status: 200 })
 }
