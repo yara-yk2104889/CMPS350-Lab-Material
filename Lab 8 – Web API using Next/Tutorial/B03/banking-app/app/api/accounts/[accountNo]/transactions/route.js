e438fd42c174b6f3c67bcbd3322215d2efc463a5
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-12 15:34:57 +0300

    up

---
+++ Lab 8 – Web API using Next/Tutorial/B03/banking-app/app/api/accounts/[accountNo]/transactions/route.js
@@ -0,0 +1,9 @@
+export async function GET(req, { params }) {
+    const message = { message: `API endpoint GET http://localhost:3000/api/accounts/${params.accountNo}/transactions` };
+    return Response.json(message, { status: 200 });
+}
+
+export async function POST(req, { params }) {
+    const message = { message: `API endpoint POST http://localhost:3000/api/accounts/${params.accountNo}/transactions` };
+    return Response.json(message, { status: 200 });
+}
